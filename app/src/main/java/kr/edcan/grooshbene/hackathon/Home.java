da42c0ca97d3a18f0b09e445dc07b1f2896ddd42
GrooshBene <wltn000129@gmail.com>
2015-07-20 21:38:08 +0900

    아이콘 안뜨던 오류 픽스

---
+++ app/src/main/java/kr/edcan/grooshbene/hackathon/Home.java
@@ -0,0 +1,68 @@
+package kr.edcan.grooshbene.hackathon;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.kakao.KakaoLink;
+import com.kakao.KakaoParameterException;
+import com.kakao.KakaoTalkLinkMessageBuilder;
+
+
+public class Home extends ActionBarActivity {
+    public KakaoLink kakaoLink;
+    public KakaoTalkLinkMessageBuilder kakaoTalkLinkMessageBuilder;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    kakaoLink = KakaoLink.getKakaoLink(getApplicationContext());
+                    kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+                    kakaoTalkLinkMessageBuilder.addText("박지수님이 당신을 지목하였습니다.");
+                    kakaoTalkLinkMessageBuilder.addImage("https://lh3.googleusercontent.com/-iSOlEUxX5E8/VazgGlcSToI/AAAAAAAAAFU/rygHzNrUVXY/w346-h221/main.png", 300, 200);
+                    kakaoTalkLinkMessageBuilder.addAppLink("앱으로 연결합니다");
+                    kakaoLink.sendMessage(kakaoTalkLinkMessageBuilder.build(), Home.this);
+                    kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+                } catch (KakaoParameterException e) {
+                    Log.e("error", e.getMessage());
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
--- app/src/main/java/kr/edcan/grooshbene/hackathon/MainActivity.java
+++ app/src/main/java/kr/edcan/grooshbene/hackathon/MainActivity.java
@@ -1,65 +1,30 @@
 package kr.edcan.grooshbene.hackathon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.kakao.KakaoLink;
-import com.kakao.KakaoParameterException;
 import com.kakao.KakaoTalkLinkMessageBuilder;
 
 public class MainActivity extends Activity {
-
-    public KakaoLink kakaoLink;
-    public KakaoTalkLinkMessageBuilder kakaoTalkLinkMessageBuilder;
-    Button btn;
-
+    LinearLayout splash;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = (Button)findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_splash);
+        splash = (LinearLayout)findViewById(R.id.splash);
+        splash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    kakaoLink = KakaoLink.getKakaoLink(getApplicationContext());
-                    kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
-                    kakaoTalkLinkMessageBuilder.addText("test");
-                    kakaoLink.sendMessage(kakaoTalkLinkMessageBuilder.build(), MainActivity.this);
-                    kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
-                } catch (KakaoParameterException e) {
-                    Log.e("error", e.getMessage());
-                }
+                intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
             }
         });
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
